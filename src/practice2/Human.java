5aba553297cf1614398dc017a582c0e2ce243fd5
Alexander Gribkov <gribkovalexander@gmail.com>
2024-02-19 20:13:02 +0300

    Practice 2 test to class

--- src/practice2/Human.java
+++ src/practice2/Human.java
@@ -31,43 +31,4 @@ public class Human {
                 ", weight=" + weight +
                 '}';
     }
-
-    public static void main(String[] args) {
-        List<Human> humans = List.of(
-                new Human(19, "Александр", "Грибков", LocalDate.of(2004, 10, 14), 65),
-                new Human(60, "Семен", "Демидов", LocalDate.of(1963, 4, 3), 105),
-                new Human(45, "Валерий", "Иванов", LocalDate.of(1966, 1, 2), 25),
-                new Human(58, "Лев", "Кокоткин", LocalDate.of(1982, 2, 25), 15),
-                new Human(22, "Егор", "Иванов", LocalDate.of(2000, 11, 4), 45),
-                new Human(35, "Геннадий", "Дуванов", LocalDate.of(1980, 12, 2), 66),
-                new Human(46, "Федор", "Балтабев", LocalDate.of(1999, 8, 25), 15),
-                new Human(10, "Юрий", "Морошкин", LocalDate.of(1950, 5, 23), 34)
-        );
-
-        System.out.println("Сортировка по весу в обратном порядке:");
-        humans.stream()
-                .sorted((human1, human2) -> Integer.compare(human2.weight, human1.weight))
-                .forEach(System.out::println);
-        System.out.println();
-
-        System.out.println("Не Ивановы:");
-        humans.stream()
-                .filter((human) -> !Objects.equals(human.lastName, "Иванов"))
-                .forEach(System.out::println);
-        System.out.println();
-
-        System.out.println("Сортировка по возрасту:");
-        humans.stream()
-                .sorted(Comparator.comparingInt(human -> human.age))
-                .forEach(System.out::println);
-        System.out.println();
-
-        System.out.println("Произведение всех возрастов:");
-        System.out.println(new DecimalFormat( "###,###" ).format(
-                humans.stream()
-                .map((human -> (long) human.age))
-                .reduce((a, b) -> a * b)
-                .get()
-        ));
-    }
 }
