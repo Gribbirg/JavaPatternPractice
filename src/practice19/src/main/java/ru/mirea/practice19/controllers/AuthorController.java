7f0562225979d22110b8a16718c5eba0fb5d0f69
Alexander Gribkov <gribkovalexander@gmail.com>
2024-03-30 20:28:37 +0300

    Practice 19

---
+++ src/practice19/src/main/java/ru/mirea/practice19/controllers/AuthorController.java
@@ -0,0 +1,47 @@
+package ru.mirea.practice19.controllers;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ru.mirea.practice19.dto.AuthorDto;
+import ru.mirea.practice19.models.Author;
+import ru.mirea.practice19.services.AuthorService;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping(value = "/authors")
+public class AuthorController {
+
+    private final AuthorService service;
+
+    @GetMapping
+    @ResponseBody
+    public List<AuthorDto> getAuthors() {
+        return service.getAllAuthors();
+    }
+
+    @PostMapping
+    @ResponseBody
+    public Author addAuthor(
+            @RequestBody Author author
+    ) {
+        service.addAuthor(author);
+        return author;
+    }
+
+    @Transactional
+    @DeleteMapping("/{id}")
+    @ResponseBody
+    public String deleteAuthor(@PathVariable Long id) {
+        return "Count: " + service.deleteAuthorById(id);
+    }
+
+    @GetMapping("/filter")
+    @ResponseBody
+    public List<AuthorDto> getFilter(@RequestBody Author author) {
+        return service.findByFilter(author);
+    }
+}
