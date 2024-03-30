37cdb2581f524c85f5e19d4d19233d8cfb3a31fd
Alexander Gribkov <gribkovalexander@gmail.com>
2024-03-30 22:42:53 +0300

    Practice 21

---
+++ src/practice21/src/main/java/ru/mirea/practice21/repositories/BookRepository.java
@@ -0,0 +1,14 @@
+package ru.mirea.practice21.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import ru.mirea.practice21.models.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+    Optional<List<Book>> getAllBy();
+
+    Optional<Integer> deleteBookById(Long id);
+}
